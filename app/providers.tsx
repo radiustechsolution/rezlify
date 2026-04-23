a2ee949f10cf2ab4735a8c99fe6b551c54985996
smhartgroup <oludowole-o@smhartgroup.com>
2026-04-23 06:44:24 +0100

    scsc

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="data-theme" defaultTheme="dark">
+    <ThemeProvider attribute="data-theme" defaultTheme="light">
       {children}
     </ThemeProvider>
   );
